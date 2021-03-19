29a60aea57504bacfe2000695c80a158c8dbf765
KrasulyaV <krasulyavova@ukr.net>
2021-03-19 17:10:25 +0200

    First Commit(added Jsoup dependencies, added test code to psvm)

--- src/main/java/com/home/allegro/Allegro.java
+++ src/main/java/com/home/allegro/Allegro.java
@@ -1,5 +1,23 @@
 package com.home.allegro;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Allegro {
+    public static void main(String[] args) {
+        Document doc;
+        String title = " ";
+        try {
+            doc = Jsoup.connect("http://google.com/").get();
+            title = doc.title();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("After parsing, Title : " + title);
+    }
+
 
 }
