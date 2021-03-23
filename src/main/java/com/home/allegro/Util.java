ef82321404f773c261aaf989ef63df9eac53219e
KrasulyaV <krasulyavova@ukr.net>
2021-03-23 11:19:19 +0200

    Updated Util

--- src/main/java/com/home/allegro/Util.java
+++ src/main/java/com/home/allegro/Util.java
@@ -8,38 +8,42 @@ import org.jsoup.select.Elements;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.home.allegro.HTMLClassName.*;
 
 public class Util {
-    private static List<Map<String, String>> productListWithDiscount = new ArrayList<>();
+
+    private Util() {
+    }
+
+    private static final Map<String, Map<String, String>> productMapWithDiscount = new HashMap<>();
 
     public static void writeToFile() {
         try (FileWriter fw = new FileWriter("NewData.csv", false);
              BufferedWriter bw = new BufferedWriter(fw)) {
             for (String url : Category.getListUrl()) {
-                bw.write(getOneHundredProductFromOneCategory(url));
-                bw.append('\n');
+                productMapWithDiscount.put(getCategoryNameFromURLString(url), getOneHundredProductFromOneCategory(url));
             }
+            bw.write(productMapWithDiscount.toString());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static String getOneHundredProductFromOneCategory(String url) {
+    public static Map<String, String> getOneHundredProductFromOneCategory(String url) {
+        Map<String, String> productsWithDiscountInfoMap = new LinkedHashMap<>();
         for (int i = 1; i < 100; i++) {
-            if (productListWithDiscount.size() < 100) {
-                addingInfoAboutDiscountProductToProductList(String.format(url, i));
+            if (productsWithDiscountInfoMap.size() < 100) {
+                addingInfoAboutDiscountProductToProductList(String.format(url, i), productsWithDiscountInfoMap);
             } else break;
         }
-        return productListWithDiscount.toString();
+        return productsWithDiscountInfoMap;
     }
 
-    public static void addingInfoAboutDiscountProductToProductList(String url) {
+    public static void addingInfoAboutDiscountProductToProductList(String url, Map<String, String> productsWithDiscountInfoMap) {
         StringBuilder sb = new StringBuilder();
 
         for (Element element : getProductElementsFromPage(url)) {
@@ -49,11 +53,9 @@ public class Util {
                 sb.append("New price: ").append(element.getElementsByClass(RIGHT_PRICE.getClassName()).text()).append("; ");
                 sb.append("Full price with delivery: ").append(element.getElementsByClass(FULL_PRICE_WITH_DELIVERY.getClassName()).text()).append("; ");
                 sb.append("People bought: ").append(getRightStringWithNumberCustomer(element.getElementsByClass(NUMBER_CUSTOMERS.getClassName()).text())).append("\n");
-                Map<String, String> productsWithDiscountInfoMap = new LinkedHashMap<>();
                 productsWithDiscountInfoMap.put(element.getElementsByClass(PRODUCT_NAME.getClassName()).text(), sb.toString());
-                productListWithDiscount.add(productsWithDiscountInfoMap);
                 sb.setLength(0);
-                if (productListWithDiscount.size() == 100) break;
+                if (productsWithDiscountInfoMap.size() == 100) break;
             }
         }
     }
@@ -79,4 +81,11 @@ public class Util {
     private static Elements getProductElementsFromPage(String url) {
         return Jsoup.connect(url).get().body().getElementsByClass("mpof_ki myre_zn _9c44d_1Hxbq");
     }
+
+    private static String getCategoryNameFromURLString(String url) {
+        String[] arraySplittingByDownHill = url.split("/");
+        String[] arraySplittingByQuestionMark = arraySplittingByDownHill[arraySplittingByDownHill.length - 1].split("\\?");
+        return arraySplittingByQuestionMark[0].toUpperCase();
+    }
+
 }
